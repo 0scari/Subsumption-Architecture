8eaef6c54b7d0289529d21b8c367277b28628e22
Oskars Pozdnakovs <opozdnakov@uni.coventry.ac.uk>
2019-03-20 19:34:15 +0000

    + untested coordinator functionality

--- functions.h
+++ functions.h
@@ -7,11 +7,7 @@
 #ifndef P3_FUNCTIONS_H
 #define P3_FUNCTIONS_H
 
-struct ResolutionInstrSet {
-    string wheel1;
-    string wheel2;
-    string wheel3;
-};
+
 
 void do_wheel_work(Wheel* wheel)
 {
@@ -20,14 +16,13 @@ void do_wheel_work(Wheel* wheel)
         lck.lock();
         cout << "Wheel " << wheel->id << " goes " << wheel->direction << endl;
         wheel_cv.wait(lck, [wheel] {
-            if (wheel->is_coordinator_signal_received) {
+            if (Wheel::is_coordinator_signal_received) {
                 return true;
             } else if (!problem_spawner::problems.empty()) {
                 problem_spawner::problem problem = problem_spawner::problems.front();
                 if (wheel->id == problem.wheel) {
                     wheel->sensor->is_activated = true;
                     sensor_cv.notify_all();
-
                 }
             }
             return false;
@@ -39,52 +34,54 @@ void do_wheel_work(Wheel* wheel)
 
 
 void resolve_rock_problem(int severity) {
+    ResolutionInstrSet instruction_set;
     if (severity > 6) {
-        ResolutionInstrSet instructSet;
-        instructSet.wheel1 = "to perform rock evasion maneuver\n";
-        instructSet.wheel2 = "to perform rock evasion maneuver\n";
-        instructSet.wheel3 = "to perform rock evasion maneuver\n";
+        instruction_set.wheel1 = "to perform rock evasion maneuver\n";
+        instruction_set.wheel2 = "to perform rock evasion maneuver\n";
+        instruction_set.wheel3 = "to perform rock evasion maneuver\n";
     } else {
-        ResolutionInstrSet instructSet;
-        instructSet.wheel1 = "forward\n";
-        instructSet.wheel2 = "forward\n";
-        instructSet.wheel3 = "forward\n";
+        instruction_set.wheel1 = "forward\n";
+        instruction_set.wheel2 = "forward\n";
+        instruction_set.wheel3 = "forward\n";
     }
+    coordinator.instuction_queue.push_back(instruction_set);
+    manager_cv.notify_one();
 }
 
 void resolve_sand_problem(int severity) {
+    ResolutionInstrSet instruction_set;
     if (severity < 4) {
-        ResolutionInstrSet instructSet;
-        instructSet.wheel1 = "forward\n";
-        instructSet.wheel2 = "forward\n";
-        instructSet.wheel3 = "forward\n";
+        instruction_set.wheel1 = "forward\n";
+        instruction_set.wheel2 = "forward\n";
+        instruction_set.wheel3 = "forward\n";
     } else if (severity < 6) {
-        ResolutionInstrSet instructSet;
-        instructSet.wheel1 = "full throttle forward\n";
-        instructSet.wheel2 = "full throttle forward\n";
-        instructSet.wheel3 = "full throttle forward\n";
+        instruction_set.wheel1 = "full throttle forward\n";
+        instruction_set.wheel2 = "full throttle forward\n";
+        instruction_set.wheel3 = "full throttle forward\n";
     } else {
         cout << "Requesting assistance from the Earth\n";
-        ResolutionInstrSet instructSet;
-        instructSet.wheel1 = "nowhere\n";
-        instructSet.wheel2 = "nowhere\n";
-        instructSet.wheel3 = "nowhere\n";
+        instruction_set.wheel1 = "nowhere\n";
+        instruction_set.wheel2 = "nowhere\n";
+        instruction_set.wheel3 = "nowhere\n";
     }
+    coordinator.instuction_queue.push_back(instruction_set);
+    manager_cv.notify_one();
 }
 
 void resolve_cliff_edge_problem(int severity) {
+    ResolutionInstrSet instruction_set;
     if (severity < 2) {
-        ResolutionInstrSet instructSet;
-        instructSet.wheel1 = "forward\n";
-        instructSet.wheel2 = "forward\n";
-        instructSet.wheel3 = "forward\n";
+        instruction_set.wheel1 = "forward\n";
+        instruction_set.wheel2 = "forward\n";
+        instruction_set.wheel3 = "forward\n";
     } else {
         cout << "Requesting assistance from the Earth\n";
-        ResolutionInstrSet instructSet;
-        instructSet.wheel1 = "nowhere\n";
-        instructSet.wheel2 = "nowhere\n";
-        instructSet.wheel3 = "nowhere\n";
+        instruction_set.wheel1 = "nowhere\n";
+        instruction_set.wheel2 = "nowhere\n";
+        instruction_set.wheel3 = "nowhere\n";
     }
+    coordinator.instuction_queue.push_back(instruction_set);
+    manager_cv.notify_one();
 }
 
 void do_sensor_work(Sensor* sensor) {
@@ -96,16 +93,30 @@ void do_sensor_work(Sensor* sensor) {
         sensor->is_activated = false;
         problem_spawner::problem problem = problem_spawner::problems.front();
         if (problem.type == "rock") {
-//            thread t(resolve_rock_problem, problem.severity);
             resolver_threads.emplace_back(resolve_rock_problem, problem.severity);
         } else if (problem.type == "sand") {
-//            thread t(resolve_sand_problem, problem.severity);
             resolver_threads.emplace_back(resolve_sand_problem, problem.severity);
         } else if (problem.type == "cliff_edge") {
-//            thread t(resolve_cliff_edge_problem, problem.severity);
             resolver_threads.emplace_back(resolve_cliff_edge_problem, problem.severity);
         };
         problem_spawner::problems.erase(problem_spawner::problems.begin());
+    }
+}
+
+void do_coordinator_work() {
+    while (true) {
+        unique_lock<std::mutex> lck(manager_mtx);
+        manager_cv.wait(lck, []{
+            return !coordinator.instuction_queue.empty();
+        });
+        coordinator.wheels[0].direction = coordinator.instuction_queue[0].wheel1;
+        coordinator.wheels[1].direction = coordinator.instuction_queue[0].wheel2;
+        coordinator.wheels[2].direction = coordinator.instuction_queue[0].wheel3;
+        Wheel::is_coordinator_signal_received = true;
+        wheel_cv.notify_all();
+        coordinator.instuction_queue.erase(coordinator.instuction_queue.begin());
+        lck.unlock();
+        Wheel::is_coordinator_signal_received = false;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -7,38 +7,36 @@ using namespace std;
 
 std::mutex wheel_mtx;
 std::mutex sensor_mtx;
-std::mutex resolver_mtx;
+std::mutex manager_mtx;
 
 std::condition_variable wheel_cv;
 std::condition_variable sensor_cv;
-std::condition_variable resolver_cv;
+std::condition_variable manager_cv;
 
 vector <thread> resolver_threads;
 
 struct Sensor {
-    int id;
     bool is_activated = false;
-//    void handle_rock_problem(int severity);
-//    void handle_sand_problem(int severity);
-//    void handle_cliff_edge_problem(int severity);
 } sensor1, sensor2, sensor3;
-//void Sensor::handle_rock_problem(int severity) {
-//    cout << "rock\n";
-//}
-//void Sensor::handle_sand_problem(int severity) {
-//    cout << "sand\n";
-//
-//}
-//void Sensor::handle_cliff_edge_problem(int severity) {
-//    cout << "cliff_edge\n";
-//}
 
 struct Wheel {
     int id;
-    bool is_coordinator_signal_received = false;
+    static bool is_coordinator_signal_received;
     string direction = "forward";
     Sensor* sensor;
 } wheel1, wheel2, wheel3;
+bool Wheel::is_coordinator_signal_received = false;
+
+struct ResolutionInstrSet {
+    string wheel1;
+    string wheel2;
+    string wheel3;
+};
+
+struct Coordinator {
+    Wheel wheels[3];
+    vector<ResolutionInstrSet> instuction_queue;
+} coordinator;
 
 #include "problem-spawner.h"
 #include "functions.h"
@@ -47,22 +45,26 @@ struct Wheel {
 
 int main(void)
 {
-    wheel1.id = 1; sensor1.id = 1; wheel1.sensor = &sensor1;
-    wheel2.id = 2; sensor2.id = 2; wheel2.sensor = &sensor2;
-    wheel3.id = 3; sensor3.id = 3; wheel3.sensor = &sensor3;
+    wheel1.id = 1; wheel1.sensor = &sensor1;
+    wheel2.id = 2; wheel2.sensor = &sensor2;
+    wheel3.id = 3; wheel3.sensor = &sensor3;
+
+    coordinator.wheels[0] = wheel1;
+    coordinator.wheels[1] = wheel2;
+    coordinator.wheels[2] = wheel3;
+
+    thread coordinator_t(do_coordinator_work);
 
     thread sensor_t_1(do_sensor_work, &sensor1);
     thread sensor_t_2(do_sensor_work, &sensor2);
     thread sensor_t_3(do_sensor_work, &sensor3);
 
-    thread thread1(do_wheel_work, &wheel1);
-    thread thread2(do_wheel_work, &wheel2);
-    thread thread3(do_wheel_work, &wheel3);
+    thread wheel_t1(do_wheel_work, &wheel1);
+    thread wheel_t2(do_wheel_work, &wheel2);
+    thread wheel_t3(do_wheel_work, &wheel3);
 
     while (true) {
         thread spawner(problem_spawner::spawn_random_each_sec);
         spawner.join();
     }
-
-    thread1.join();
 }
